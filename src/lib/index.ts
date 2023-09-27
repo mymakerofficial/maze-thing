5a1f273236a9d50c470d86e164076392f0924a76
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-09-27 14:02:41 +0200

    implemented astar

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,151 @@
-// place files you want to import through the `$lib` alias in this folder.
+
+export type Nullable<T> = T | null;
+
+export interface Node {
+    x: number;
+    y: number;
+    gScore: number; // distance from start
+    hScore: number; // distance from end
+    fScore: number // gScore + hScore
+    open: boolean;
+    closed: boolean;
+    cameFrom: Nullable<Node>;
+}
+
+export type Grid = Array<Array<Node>>;
+
+export function createNode(x: number, y: number): Node {
+    return {
+        x,
+        y,
+        gScore: Infinity,
+        hScore: 0,
+        fScore: 0,
+        open: false,
+        closed: false,
+        cameFrom: null
+    };
+}
+
+function get(grid: Grid, x: number, y: number): Node {
+    return grid[x][y];
+}
+
+function getLowestFScoreOf(openSet: Set<Node>): Nullable<Node> {
+    return Array.from(openSet).reduce((a, b) => a.fScore! < b.fScore! ? a : b);
+}
+
+function getNeighbors(grid: Grid, node: Node): Array<Node> {
+    const neighbors = new Array<Node>;
+
+    const x = node.x;
+    const y = node.y;
+
+    if (x > 0) {
+        neighbors.push(get(grid, x - 1, y));
+    }
+
+    if (x < grid.length - 1) {
+        neighbors.push(get(grid, x + 1, y));
+    }
+
+    if (y > 0) {
+        neighbors.push(get(grid, x, y - 1));
+    }
+
+    if (y < grid[0].length - 1) {
+        neighbors.push(get(grid, x, y + 1));
+    }
+
+    return neighbors;
+}
+
+function distance(a: Node, b: Node): number {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
+function heuristic(neighbor: Node, endNode: Node) {
+    // return Math.abs(neighbor.x - endNode.x) + Math.abs(neighbor.y - endNode.y);
+    return distance(neighbor, endNode);
+}
+
+export function createPathfinder(grid: Grid, startX: number, startY: number, endX: number, endY: number) {
+    const openSet = new Set<Node>();
+    const closedSet = new Set<Node>();
+
+    const startNode = get(grid, startX, startY);
+    const endNode = get(grid, endX, endY);
+
+    openSet.add(startNode); startNode.open = true;
+    startNode.gScore = 0;
+
+    function step() {
+        if (openSet.size === 0) {
+            // no solution
+            return false;
+        }
+
+        // current node is the node in the open set with the lowest f score
+        const currentNode = getLowestFScoreOf(openSet)!;
+
+        if (currentNode === endNode) {
+            // we have reached the end
+            return true;
+        }
+
+        openSet.delete(currentNode); currentNode.open = false;
+        closedSet.add(currentNode); currentNode.closed = true;
+
+        const neighbors = getNeighbors(grid, currentNode);
+
+        for (const neighbor of neighbors) {
+            if (closedSet.has(neighbor)) {
+                // if the neighbor is in the closed set, it has already been evaluated, so we can ignore it
+                continue;
+            }
+
+            // distance from start to neighbor of current node
+            const tentativeGScore = currentNode.gScore + distance(currentNode, neighbor);
+
+            if (!openSet.has(neighbor)) {
+                // we have discovered a new node, add it to the open set
+                openSet.add(neighbor); neighbor.open = true;
+            }
+
+            if (tentativeGScore < neighbor.gScore) {
+                // we found the best path here
+                neighbor.cameFrom = currentNode;
+                neighbor.gScore = tentativeGScore;
+                neighbor.hScore = heuristic(neighbor, endNode);
+                neighbor.fScore = neighbor.gScore + neighbor.hScore;
+            }
+        }
+
+        return null
+    }
+
+    function getPath() {
+        const path = new Array<Node>();
+
+        let currentNode = endNode;
+
+        while (currentNode !== startNode) {
+            path.push(currentNode);
+            currentNode = currentNode.cameFrom!;
+        }
+
+        path.push(startNode);
+
+        return path.reverse();
+    }
+
+    return {
+        step,
+        getPath,
+        grid,
+        startNode,
+        endNode,
+        openSet,
+        closedSet,
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,69 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script lang="ts">
+    import {createNode, createPathfinder} from "$lib";
+    import type {Node} from "$lib";
+    import type {Grid} from "$lib";
+
+    let grid = new Array<Array<Node>>(10) as Grid;
+
+    for (let x = 0; x < grid.length; x++) {
+        grid[x] = new Array<Node>(10);
+
+        for (let y = 0; y < grid[x].length; y++) {
+            grid[x][y] = createNode(x, y);
+        }
+    }
+
+    let path = new Array<Node>();
+
+    const pathfinder = createPathfinder(grid, 3, 3, 6, 8);
+
+    function step () {
+        const res = pathfinder.step();
+        grid = pathfinder.grid
+        if (res) {
+            path = pathfinder.getPath();
+        }
+    }
+</script>
+
+<main class="p-6 flex flex-col gap-4">
+    <section class="flex flex-row gap-2">
+        <button on:click={step} class="text-md py-2 px-4 rounded-sm bg-neutral-200">step</button>
+    </section>
+    <section>
+        <div class="flex flex-row gap-2">
+            {#each grid as col}
+                <div class="flex flex-col gap-2">
+                    {#each col as node}
+                        <div
+                            data-open={node.open}
+                            data-closed={node.closed}
+                            data-path={path.includes(node)}
+                            data-end={node === pathfinder.endNode}
+                            class="w-12 h-12 rounded-sm flex justify-center items-center gap-1 bg-neutral-200 data-[open=true]:bg-green-400 data-[closed=true]:bg-rose-400 data-[end=true]:bg-amber-300 data-[path=true]:!bg-blue-400"
+                        >
+                            {#if node.gScore !== Infinity}
+                                <div>
+                                    { node.gScore }
+                                </div>
+                            {/if}
+                            {#if node.cameFrom != null}
+                                <div>
+                                    {#if node.cameFrom.x < node.x}
+                                        🡄
+                                    {:else if node.cameFrom.x > node.x}
+                                        🡆
+                                    {:else if node.cameFrom.y < node.y}
+                                        🡅
+                                    {:else if node.cameFrom.y > node.y}
+                                        🡇
+                                    {/if}
+                                </div>
+                            {/if}
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
+    </section>
+</main>
